7d84436417bce7fe56669641e24ee22f92145e54
Glowbolt <glowbolt@gmail.com>
2025-07-16 13:21:34 +0300

    made ship move, created groups

--- src/main.py
+++ src/main.py
@@ -1,18 +1,34 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        updatables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+
 
         pygame.display.flip()
-        
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
